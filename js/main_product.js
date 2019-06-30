664a2e45334760865697eb601da5f0930d451033
thynguyen96 <thy.ngtt@gmail.com>
2019-07-01 00:15:21 +0700

    first commit

---
+++ js/main_product.js
@@ -0,0 +1,17 @@
+// $('.product_img').mouseover(function () {
+//     console.log("moveover")
+//     $('.product_button_group').css(
+//     {
+//     visibility: visible,
+//     }, 150);
+// }).mouseout(function () {
+//     console.log("moveout")
+//     $('.product_button_group').css({ visibility: hidden }, 150);
+// });
+
+// $(".product_img").mouseout(function() {
+//     console.log("moveover")
+//   }).mouseover(function() {
+//     console.log("moveout")
+// });
+ 
---
+++ js/side_nav.js
@@ -0,0 +1,12 @@
+function openSideNav(){
+    document.getElementById('sideNav').style.width = "300px";
+    document.getElementById('mainContainer').style.left = "300px";
+    // document.getElementById('body').style.left = "300px";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeSideNav(){
+    document.getElementById('sideNav').style.width = "0";
+    document.getElementById('mainContainer').style.left = "0";
+    // document.body.style.backgroundColor = "white";
+}
