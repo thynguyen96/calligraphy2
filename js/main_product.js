315b7240d16e90a8bfa62c9e23032136821ca504
thynguyen96 <thy.ngtt@gmail.com>
2019-07-04 23:05:21 +0700

    Adding rate star

--- js/main_product.js
+++ js/main_product.js
@@ -1,17 +1,37 @@
-// $('.product_img').mouseover(function () {
-//     console.log("moveover")
-//     $('.product_button_group').css(
-//     {
-//     visibility: visible,
-//     }, 150);
-// }).mouseout(function () {
-//     console.log("moveout")
-//     $('.product_button_group').css({ visibility: hidden }, 150);
-// });
+$(document).ready(function(){
+    $('#stars li').on('mouseover', function(){
+        var onStar = parseInt($(this).data('value'), 10);
+        console.log(onStar);
 
-// $(".product_img").mouseout(function() {
-//     console.log("moveover")
-//   }).mouseover(function() {
-//     console.log("moveout")
-// });
- 
+        $(this).parent().children('li.star').each(function(e){
+
+            if(e < onStar){
+                $(this).addClass('hover');
+                console.log(e);
+            }
+            else{
+                $(this).removeClass('hover');
+            }
+
+        })
+    })
+    .on('mouseout', function(){
+        $(this).parent().children('li.star').each(function(e){
+            $(this).removeClass('hover');
+        })
+    })
+
+    $('#stars li').on('click', function(){
+        var onStar = parseInt($(this).data('value'), 10);
+        var stars =  $(this).parent().children('li.star');
+
+        for(var i = 0; i < stars.length; i++){
+            $(stars[i]).removeClass('selected');
+        }
+
+        for(var i = 0; i < onStar; i++){
+            $(stars[i]).addClass('selected');
+        }
+    })
+
+});
